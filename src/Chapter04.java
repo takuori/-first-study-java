12b0c14908a5f70d61bf1f73054529203a13953d
MCZ-s <104710179+takuori@users.noreply.github.com>
2022-08-23 10:50:55 +0900

    [4thCommit]変数処理修正

--- src/Chapter04.java
+++ src/Chapter04.java
@@ -1,16 +1,12 @@
 
 public class Chapter04 {
 	public static void main(String[] args) {
-		String str1;
-		str1 = "さとうたくみ";
+		final int LUCKY_NUMBER = 7;
+		String str1 = "さとうたくみ";
 		System.out.println(str1);
-		
 		str1 = "ミニオン";
 		System.out.println(str1);
-		
-		final int LUCKY_NUMBER = 7;
 		System.out.println(LUCKY_NUMBER);
-		
 	}
 
 }
