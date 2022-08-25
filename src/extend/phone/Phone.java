1dab3c07b417f5ba9fe2c848ec92d20cd5a2ac4f
MCZ-s <104710179+takuori@users.noreply.github.com>
2022-08-25 15:08:15 +0900

    [16thCommit]ポリモーフィズム理解

--- src/extend/phone/Phone.java
+++ src/extend/phone/Phone.java
@@ -4,9 +4,13 @@ package extend.phone;
 public interface Phone {
 	
 	//フィールドの宣言(定数)
-	int MAX_NUMBER_DIGITS = 11;
+	final int MAX_NUMBER_DIGITS = 11;
 	
 	//public abstractが自動で追加される（抽象メソッド）
-	public void call(String number);
+	void call(String number);
+	
+	default void powerOff() {
+		System.out.println("電源を切ります");
+	}
 
 }
---
+++ src/practice/Flying.java
@@ -0,0 +1,11 @@
+package practice;
+
+public interface Flying {
+	
+	void fly();
+	
+	default void powerOff() {
+		System.out.println("飛んでいる場合は落下します");
+	}
+
+}
