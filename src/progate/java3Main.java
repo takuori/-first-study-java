3962f1bbea6ee4b379c9651afa0ef37ff364bfe8
MCZ-s <104710179+takuori@users.noreply.github.com>
2022-08-26 17:36:02 +0900

    [20thCommit]Progate道場1

---
+++ src/progate/java3Main.java
@@ -0,0 +1,47 @@
+package progate;
+
+import java.util.Scanner;
+
+public class java3Main {
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		
+		System.out.print("何人分の情報を入力しますか：");
+		int n = scanner.nextInt(); 
+		
+		int maxAge = 0;
+		int sumAge = 0;
+		
+		for (int i = 0; i < n; i++) {
+			System.out.print( i + 1 + "人目");
+			
+			System.out.print("名前：");
+			String firstName = scanner.next();
+			
+			System.out.print("名字：");
+			String lastName = scanner.next();
+			
+			System.out.print("年齢：");
+			int age = scanner.nextInt();
+			
+			if (maxAge < age) {
+				maxAge = age;
+			}
+			
+			sumAge = sumAge + age;
+			
+			System.out.print("身長(m)：");
+			double height = scanner.nextDouble();
+			
+			System.out.print("体重(kg)：");
+			double weight = scanner.nextDouble();
+			
+			java3Person.printData(firstName, lastName, age, height, weight);
+		}
+		
+		System.out.println("最高年齢は" + maxAge + "歳です");
+		System.out.println("平均年齢は" + sumAge / 2 + "歳です");
+		
+	}
+
+}
