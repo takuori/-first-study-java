478e55960aed7b32c8b6bdab69c5f323e7a036ef
MCZ-s <104710179+takuori@users.noreply.github.com>
2022-08-29 16:22:02 +0900

    [24thCommit]内部クラスについて

---
+++ src/practice/Human.java
@@ -0,0 +1,10 @@
+package practice;
+
+public class Human {
+	public class HumanLeg {
+		public void kick() {
+			System.out.println("蹴る");
+		}
+	}
+
+}
---
+++ src/practice/HumanMain.java
@@ -0,0 +1,14 @@
+package practice;
+
+public class HumanMain {
+	public static void main(String[] args) {
+		
+		//Human human = new Human();
+		//Human.HumanLeg leg = human.new HumanLeg();
+		
+		//leg.kick();
+		
+		new Human().new HumanLeg().kick();
+	}
+
+}
